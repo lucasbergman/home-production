4b6225b8f6708628aa320f0400b4b754bcbdb613
Lucas Bergman <lucas@bergmans.us>
2022-11-22 16:03:42 +0000

    linode: Make instance setup script to APT upgrade

--- assets/instancesetup.sh
+++ assets/instancesetup.sh
@@ -2,5 +2,13 @@
 # <UDF name="creds" label="base64-JSON-encoded GCP credentials" example="ewogIC..." default="">
 set -e
 exec &> /root/stackscript.log
+
 /usr/bin/install -m 640 -o 0 -g 0 /dev/null /etc/gcp-instance-creds.json
 echo -n "$CREDS" | /usr/bin/base64 --decode > /etc/gcp-instance-creds.json
+
+export DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical
+/usr/bin/apt-get --quiet --assume-yes update
+/usr/bin/apt-get --quiet --assume-yes \
+    --option "Dpkg::Options::=--force-confdef" \
+    --option "Dpkg::Options::=--force-confnew" \
+    upgrade
--- assets/linode.tf
+++ assets/linode.tf
@@ -1,7 +1,7 @@
 resource "linode_stackscript" "write_gcp_creds" {
   label       = "write_gcp_creds"
   description = "Writes a GCP credentials file"
-  script      = file("writegcpcreds.sh")
+  script      = file("instancesetup.sh")
   images      = ["any/all"]
   is_public   = false
 }
