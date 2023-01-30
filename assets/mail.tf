c785b35abbab28bdc0d52fce2dd7e68c9321c8aa
Lucas Bergman <lucas@bergmans.us>
2023-01-30 14:17:46 +0000

    mail: Add bergmans.us and blurt.chat to SES

--- assets/mail.tf
+++ assets/mail.tf
@@ -20,3 +20,37 @@ resource "google_dns_record_set" "bergmanhouse_ses_dkim" {
   rrdatas      = ["${aws_ses_domain_dkim.bergmanhouse.dkim_tokens[count.index]}.dkim.amazonses.com."]
   ttl          = 3600
 }
+
+resource "aws_ses_domain_identity" "bergmans" {
+  domain = "bergmans.us"
+}
+
+resource "aws_ses_domain_dkim" "bergmans" {
+  domain = aws_ses_domain_identity.bergmans.domain
+}
+
+resource "google_dns_record_set" "bergmans_ses_dkim" {
+  count        = 3
+  managed_zone = google_dns_managed_zone.bergmans.name
+  name         = "${aws_ses_domain_dkim.bergmans.dkim_tokens[count.index]}._domainkey.bergmans.us."
+  type         = "CNAME"
+  rrdatas      = ["${aws_ses_domain_dkim.bergmans.dkim_tokens[count.index]}.dkim.amazonses.com."]
+  ttl          = 3600
+}
+
+resource "aws_ses_domain_identity" "blurt" {
+  domain = "blurt.chat"
+}
+
+resource "aws_ses_domain_dkim" "blurt" {
+  domain = aws_ses_domain_identity.blurt.domain
+}
+
+resource "google_dns_record_set" "blurt_ses_dkim" {
+  count        = 3
+  managed_zone = google_dns_managed_zone.blurt.name
+  name         = "${aws_ses_domain_dkim.blurt.dkim_tokens[count.index]}._domainkey.blurt.chat."
+  type         = "CNAME"
+  rrdatas      = ["${aws_ses_domain_dkim.blurt.dkim_tokens[count.index]}.dkim.amazonses.com."]
+  ttl          = 3600
+}
